1034f6d2a230c89fa09be82e8fc894245b2f71a5
MerazMz <merazhaque74663@gmail.com>
2025-12-06 21:43:00 +0530

    fix api key

--- frontend/src/config/firebaseConfig.js
+++ frontend/src/config/firebaseConfig.js
@@ -6,13 +6,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDLIwSyBqmWphUtnpOUHAidlzzOrQ_7PRY",
-    authDomain: "prolance-r.firebaseapp.com",
-    projectId: "prolance-r",
-    storageBucket: "prolance-r.firebasestorage.app",
-    messagingSenderId: "329509169758",
-    appId: "1:329509169758:web:d9dc8cb7f9928e8fc962cf",
-    measurementId: "G-D6YC7WT39B"
+    apiKey: import.meta.env.FIREBASE_API_KEY,
+    authDomain: import.meta.env.FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.FIREBASE_APP_ID,
+    measurementId: import.meta.env.FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
